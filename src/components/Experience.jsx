622781a59f52c18f8e5afb8ef47e1acfda171c91
smasoudhosseini <smasoudhosseini@alum.sharif.edu>
2022-04-10 15:21:07 +0430

    second commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,6 +6,15 @@ const Experience = () => {
           <h2 className="mb-5">Experience</h2>
           <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="flex-grow-1">
+              <h3 className="mb-0">Cryptography and Security Expert</h3>
+              <div className="subheading mb-3">ICT</div>
+            </div>
+            <div className="flex-shrink-0">
+              <span className="text-p">2022 - Present</span>
+            </div>
+          </div>
+          <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div className="flex-grow-1">
               <h3 className="mb-0">
                 <a href="http://math.sharif.ir/staff/">
                   Assistant Site Manager
@@ -18,7 +27,7 @@ const Experience = () => {
               </div>
             </div>
             <div className="flex-shrink-0">
-              <span className="text-p">2019 - Present</span>
+              <span className="text-p">2019 - 2022</span>
             </div>
           </div>
           <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
